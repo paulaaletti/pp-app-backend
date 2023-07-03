7c2c63e89d13679920cea35a83359a4b3ddaa988
paulaaletti <82065887+paulaaletti@users.noreply.github.com>
2023-07-03 12:24:09 -0300

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -3,12 +3,12 @@ const Sequelize = require("sequelize");
 const fs = require('fs');
 const path = require('path');
 
-const sequelize = new Sequelize(config.DB,
-                                config.USER,
-                                config.PASSWORD,
+const sequelize = new Sequelize(process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
                                 {
-                                    host: config.HOST,
-                                    port: config.PORT,
+                                    host: process.env.DB_HOST,
+                                    port: process.env.DB_PORT,
                                     dialect: config.dialect,
                                     // ssl: config.SSL == "true",
                                     dialectOptions: {
