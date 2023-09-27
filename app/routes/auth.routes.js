b141c904566f76b6affda67f2370ac82325a5dd5
MaleBurs <maleburs@gmail.com>
2023-09-27 16:36:03 -0300

    bug

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -34,7 +34,7 @@ module.exports = function(app) {
   app.post("/api/auth/signout", controller.signout);
   app.post("/api/auth/refreshtoken", controller.refreshToken);
   app.post("/api/auth/sendMailTokenToResetPassword",tokenController.sendMailTokenToResetPassword);
-  api.post("/resetPassword",passwordController.resetPassword);
+  api.post("/resetPassword/:token",passwordController.resetPassword);
   app.post("/api/auth/updatePasswordViaEmail",passwordController.updatePasswordViaEmail);
   app.post("/api/auth/changePasswordViaSettings",passwordController.changePasswordViaSettings);
   app.post("/api/auth/findUserById", controller.findUserById);
